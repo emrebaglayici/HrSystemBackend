1a49ef3f0eea4c60dcd18eadd62e26b03de948fa
emre <emrebaglayici1@gmail.com>
2022-08-30 17:27:25 +0300

    Step algorithm done.

--- src/main/java/com/emrebaglayici/myhremrebaglayici/Business/Concretes/StepManager.java
+++ src/main/java/com/emrebaglayici/myhremrebaglayici/Business/Concretes/StepManager.java
@@ -11,7 +11,6 @@ import com.emrebaglayici.myhremrebaglayici.Exceptions.FillTheBlanksException;
 import com.emrebaglayici.myhremrebaglayici.Exceptions.NotFountException;
 import com.emrebaglayici.myhremrebaglayici.Repository.StepRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -35,8 +34,8 @@ public class StepManager implements StepService {
             throw new NotFountException("Step must be true at first");
         if (step.toStep().getNotes().isEmpty() || step.toStep().getName().isEmpty())
             throw new FillTheBlanksException("Please fill all blanks");
-        Optional<Application> applyOptional=this.applicationCheckManager.findById(step.toStep().getApplicationId());
-        Application application=applyOptional.orElseThrow(()->new NotFountException("Application not found"));
+        Optional<Application> applyOptional = this.applicationCheckManager.findById(step.toStep().getApplicationId());
+        Application application = applyOptional.orElseThrow(() -> new NotFountException("Application not found"));
         if (!step.toStep().getName().equals(Steps.HR.getName()))
             throw new NotFountException("Step must begin with HR");
         step.toStep().setOrderCount(1);
@@ -45,48 +44,63 @@ public class StepManager implements StepService {
 
     @Override
     public Step updateStep(Step step) {
-        Optional<Step> stepOptional=this.stepRepository.findById(step.getId());
-        Step steps=stepOptional.orElseThrow(()->new NotFountException("Step not found!"));
+        Optional<Step> stepOptional = this.stepRepository.findById(step.getId());
+        Step steps = stepOptional.orElseThrow(() -> new NotFountException("Step not found!"));
 
-        Optional<Application> applicationOptional=this.applicationCheckManager.findById(step.getApplicationId());
-        Application application=applicationOptional.orElseThrow(()->new NotFountException("Application not found"));
+        Optional<Application> applicationOptional = this.applicationCheckManager.findById(step.getApplicationId());
+        Application application = applicationOptional.orElseThrow(() -> new NotFountException("Application not found"));
 
-        Optional<JobAdvertisement> jobAdvertisementOptional=this.jobAdvertisementCheckService.getJobById(application.getJobId());
-        JobAdvertisement jobAdvertisement=jobAdvertisementOptional.orElseThrow(()->new NotFountException("Job Advertisement not found"));
+        Optional<JobAdvertisement> jobAdvertisementOptional = this.jobAdvertisementCheckService.getJobById(application.getJobId());
+        JobAdvertisement jobAdvertisement = jobAdvertisementOptional.orElseThrow(() -> new NotFountException("Job Advertisement not found"));
 
-        if (!step.isResult())
-            throw new NotFountException("Steps are over because interview fails");
+
         if (step.getName().equals(Steps.HR.getName()))
             throw new NotFountException("Hr can only in first step");
-        if (steps.getOrderCount()>jobAdvertisement.getInterviewCount())
+        if (steps.getOrderCount() >= jobAdvertisement.getInterviewCount())
             throw new NotFountException("Steps count reached, you cannot add any step");
-        if (steps.getOrderCount()<jobAdvertisement.getInterviewCount()){
+        if (!steps.isResult()) {
+            steps.setName(step.getName());
+            steps.setResult(step.isResult());
+            steps.setNotes(step.getNotes());
+            steps.setOrderCount(jobAdvertisement.getInterviewCount() + 1);
+            log.info("Steps are over because interview fails");
+            return steps;
+        }
+
+        if (steps.getOrderCount() < jobAdvertisement.getInterviewCount() - 1) {
             log.info(steps.getOrderCount().toString());
-            if (!step.getName().equals(Steps.OFFER.getName())){
+            if (!step.getName().equals(Steps.OFFER.getName())) {
                 steps.setName(step.getName());
                 steps.setResult(step.isResult());
                 steps.setNotes(step.getNotes());
-                steps.setOrderCount(steps.getOrderCount()+1);
+                steps.setOrderCount(steps.getOrderCount() + 1);
                 log.info(steps.getOrderCount().toString());
                 this.stepRepository.save(steps);
                 return steps;
             }
-            if (step.getName().equals(Steps.OFFER.getName()))
-                throw new NotFountException("Offer must be last step");
-        }
-        if (steps.getOrderCount()==jobAdvertisement.getInterviewCount()){
-            log.info(steps.getOrderCount().toString());
-            if (step.getName().equals(Steps.OFFER.getName())){
+            if (step.getName().equals(Steps.OFFER.getName())) {
                 steps.setName(step.getName());
                 steps.setResult(step.isResult());
                 steps.setNotes(step.getNotes());
-                steps.setOrderCount(steps.getOrderCount()+1);
-                log.info(steps.getOrderCount().toString());
+                steps.setOrderCount(jobAdvertisement.getInterviewCount() + 1);
+                log.info("Offer given to interviewer and steps are done.");
                 this.stepRepository.save(steps);
                 return steps;
-            }if (!step.getName().equals(Steps.OFFER.getName()))
-                throw new NotFountException("Offer must be last step");
+            }
+
         }
+        log.info(steps.getOrderCount().toString());
+        if (step.getName().equals(Steps.OFFER.getName())) {
+            steps.setName(step.getName());
+            steps.setResult(step.isResult());
+            steps.setNotes(step.getNotes());
+            steps.setOrderCount(steps.getOrderCount() + 1);
+            log.info(steps.getOrderCount().toString());
+            this.stepRepository.save(steps);
+            return steps;
+        }
+        if (!step.getName().equals(Steps.OFFER.getName()))
+            throw new NotFountException("Offer must be last step");
         return steps;
     }
 }
